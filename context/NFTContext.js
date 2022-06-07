a62b945a5da3692b71934e1f397346c7ea328eab
Pablo Acebedo <pablofelipe@me.com>
2022-06-07 16:31:20 -0500

    First NFT uploaded

--- context/NFTContext.js
+++ context/NFTContext.js
@@ -2,8 +2,11 @@ import React, { useEffect, useState } from 'react';
 import Web3Modal from 'web3modal';
 import { ethers } from 'ethers';
 import axios from 'axios';
+import { create as ipfsHttpClient } from 'ipfs-http-client';
 
 import { MarketAddress, MarketAddressABI } from './constants';
+
+const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0');
 
 export const NFTContext = React.createContext();
 
@@ -35,8 +38,20 @@ export const NFTProvider = ({ children }) => {
     window.location.reload();
   };
 
+  const uploadToIPFS = async (file, setFileUrl) => {
+    try {
+      const added = await client.add({ content: file });
+
+      const url = `https://ipfs.infura.io/ipfs/${added.path}`;
+
+      return url;
+    } catch (error) {
+      console.log('error uploading file');
+    }
+  };
+
   return (
-    <NFTContext.Provider value={{ nftCurrency, connectWallet, currentAccount }}>
+    <NFTContext.Provider value={{ nftCurrency, connectWallet, currentAccount, uploadToIPFS }}>
       { children }
     </NFTContext.Provider>
   );
--- pages/create-nft.js
+++ pages/create-nft.js
@@ -4,6 +4,7 @@ import { useDropzone } from 'react-dropzone';
 import Image from 'next/image';
 import { useTheme } from 'next-themes';
 
+import { NFTContext } from '../context/NFTContext';
 import { Button, Input } from '../components';
 import images from '../assets';
 
@@ -11,8 +12,12 @@ const CreateNFT = () => {
   const [fileUrl, setFileUrl] = useState(null);
   const [formInput, setFormInput] = useState({ price: '', name: '', description: '' });
   const { theme } = useTheme();
+  const { uploadToIPFS } = useContext(NFTContext);
 
-  const onDrop = useCallback(() => {
+  const onDrop = useCallback(async (acceptedFile) => {
+    const url = await uploadToIPFS(acceptedFile[0]);
+    console.log({ url });
+    setFileUrl(url);
     // await uploadToInfura(acceptedFile[0]);
   }, []);
 
