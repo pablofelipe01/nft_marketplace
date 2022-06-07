65e20f8e61cee5e1f270d6b7d56f523095917ce1
Pablo Acebedo <pablofelipe@me.com>
2022-06-07 15:27:37 -0500

    Meta Mask connected

--- context/NFTContext.js
+++ context/NFTContext.js
@@ -9,9 +9,34 @@ export const NFTContext = React.createContext();
 
 export const NFTProvider = ({ children }) => {
   const nftCurrency = 'MATIC';
+  const [currentAccount, setCurrentAccount] = useState('');
+
+  const checkIfWalletIsConnect = async () => {
+    if (!window.ethereum) return alert('Please install MetaMask.');
+
+    const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+    if (accounts.length) {
+      setCurrentAccount(accounts[0]);
+    } else {
+      console.log('No accounts found');
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnect();
+  }, []);
+
+  const connectWallet = async () => {
+    if (!window.ethereum) return alert('Please install MetaMask.');
+
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+
+    setCurrentAccount(accounts[0]);
+    window.location.reload();
+  };
 
   return (
-    <NFTContext.Provider value={{ nftCurrency }}>
+    <NFTContext.Provider value={{ nftCurrency, connectWallet, currentAccount }}>
       { children }
     </NFTContext.Provider>
   );
