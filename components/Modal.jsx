94be8e3429801324af6f5afd1e278c929aae5a4a
Pablo Acebedo <pablofelipe@me.com>
2022-06-17 17:51:58 -0500

    Modal is ready

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -8,11 +8,17 @@ const Modal = ({ header, body, footer, handleClose }) => {
   const modalRef = useRef(null);
   const { theme } = useTheme();
 
+  const handleClickOutside = (e) => {
+    if (modalRef.current && !modalRef.current.contains(e.target)) {
+      handleClose();
+    }
+  };
+
   return (
-    <div className="flexCenter fixed inset-0 z-10 bg-overlay-black animated fadeIn">
+    <div onClick={handleClickOutside} className="flexCenter fixed inset-0 z-10 bg-overlay-black animated fadeIn">
       <div ref={modalRef} className="w-2/5 md:w-11/12 minlg:w-2/4 dark:bg-nft-dark bg-white flex flex-col rounded-lg">
         <div className="flex justify-end mt-4 mr-4 minlg:mt-6 minlg:mr-6">
-          <div className="relative w-3 h-3 minlg:w-6 minlg:h-6 cursor-pointer">
+          <div className="relative w-3 h-3 minlg:w-6 minlg:h-6 cursor-pointer" onClick={handleClose}>
             <Image src={images.cross} layout="fill" className={theme === 'light' ? 'filter invert' : undefined} />
           </div>
         </div>
--- pages/nft-details.js
+++ pages/nft-details.js
@@ -13,6 +13,25 @@ const PaymentBodyCmp = ({ nft, nftCurrency }) => (
       <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-base minlg:text-xl">Item</p>
       <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-base minlg:text-xl">Subtotal</p>
     </div>
+    <div className="flexBetweenStart my-5">
+      <div className="flex-1 flexStartCenter">
+        <div className="relative w-28 h-28">
+
+          <Image src={nft.image} layout="fill" objectFit="cover" />
+        </div>
+        <div className="flexCenterStart flex-col ml-5">
+          <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-sm minlg:text-xl">{shortenAddress(nft.seller)}</p>
+          <p className="font-poppins dark:text-white text-nft-black-1 text-sm minlg:text-xl font-normal">{nft.name}</p>
+        </div>
+      </div>
+      <div>
+        <p className="font-poppins dark:text-white text-nft-black-1 text-sm minlg:text-xl font-normal">{nft.price} <span className="font-semibold">{nftCurrency}</span></p>
+      </div>
+    </div>
+    <div className="flexBetween mt-10">
+      <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-base minlg:text-xl">Total</p>
+      <p className="font-poppins dark:text-white text-nft-black-1 text-base minlg:text-xl font-normal">{nft.price} <span className="font-semibold">{nftCurrency}</span></p>
+    </div>
 
   </div>
 
@@ -21,6 +40,7 @@ const PaymentBodyCmp = ({ nft, nftCurrency }) => (
 const NFTDetails = () => {
   const { currentAccount, nftCurrency } = useContext(NFTContext);
   const [isLoading, setIsLoading] = useState(true);
+  const [paymentModal, setPaymentModal] = useState(false);
   const [nft, setNft] = useState({ image: '', itemId: '', name: '', owner: '', price: '', seller: '' });
   const router = useRouter();
 
@@ -74,23 +94,34 @@ const NFTDetails = () => {
                 btnName={`Buy for ${nft.price} ${nftCurrency}`}
                 btnType="primary"
                 classStyles="mr-5 sm:mr-0 sm:mb-5 rounded-xl"
-                  // handleClick={() => setPaymentModal(true)}
+                handleClick={() => setPaymentModal(true)}
               />
 
             )}
 
         </div>
       </div>
+      {paymentModal && (
       <Modal
         header="Check Out"
         body={<PaymentBodyCmp nft={nft} nftCurrency={nftCurrency} />}
         footer={(
           <div className="flex flex-row sm:flex-col">
-            Custom Footer
+            <Button
+              btnName="Checkout"
+              classStyles="mr-5 sm:mb-5 sm:mr-0 rounded-xl"
+              handleClick={() => {}}
+            />
+            <Button
+              btnName="Cancel"
+              classStyles="rounded-xl"
+              handleClick={() => setPaymentModal(false)}
+            />
           </div>
         )}
-        handleClose={() => {}}
+        handleClose={() => setPaymentModal(false)}
       />
+      )}
     </div>
 
   );
